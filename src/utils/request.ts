2ec64fb7c9bebe5d1e205175a44b920ddac303d3
yang <844093580@qq.com>
2026-04-09 16:27:04 +0800

    gai address6

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -3,48 +3,54 @@ import { ElMessage } from 'element-plus'
 import useUserStore from '@/store/modules/user.ts'
 
 const request = axios.create({
-    baseURL: import.meta.env.VITE_APP_BASE_API, // import.meta.env.VITE_APP_BASE_API
+    baseURL: import.meta.env.VITE_APP_BASE_API,
     timeout: 5000
 })
 
-// 添加请求拦截器
+// 请求拦截器
 request.interceptors.request.use((config) => {
-    // 在发送请求之前做些什么
     const userStore = useUserStore()
-    config.headers.token = userStore.token // 携带token发送请求
-    return config;
+    if (userStore.token) {
+        config.headers.token = userStore.token
+    }
+    return config
 })
 
-// 添加响应拦截器
-request.interceptors.response.use((response) => {
-    // 对响应数据做点什么
-    return response.data;
-}, function (error) {
-    // 对响应错误做点什么
-    let message = '';
-    const status = error.response.status
+// 响应拦截器（修复后）
+request.interceptors.response.use(
+    (response) => {
+        return response.data
+    },
+    (error) => {
+        // 重点：加判断！！！
+        let message = ''
+        if (error.response) {
+            // 有响应状态码
+            const status = error.response.status
+            switch (status) {
+                case 401:
+                    message = 'Token过期'
+                    break
+                case 403:
+                    message = '无权访问'
+                    break
+                case 404:
+                    message = '请求地址错误'
+                    break
+                case 500:
+                    message = '服务器出现问题'
+                    break
+                default:
+                    message = '网络错误'
+            }
+        } else {
+            // 无响应（服务器挂了/网络异常）
+            message = '网络异常或服务器连接失败'
+        }
 
-    switch (status) {
-        case 401:
-            message = "Token过期"
-            break;
-        case 403:
-            message = "无权访问"
-            break;
-        case 404:
-            message = "请求地址错误"
-            break;
-        case 500:
-            message = "服务器出现问题"
-            break;
-        default:
-            message = "网络错误"
-            break;
+        ElMessage.error(message)
+        return Promise.reject(error)
     }
-    // 错误提示信息
-    ElMessage.error(message)
-
-    return Promise.reject(error);
-});
+)
 
 export default request
