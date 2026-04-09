48bdab416ce860bc0134580faadb48f56da0b975
yang <844093580@qq.com>
2026-04-09 15:37:47 +0800

    gai address2

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig(({ command, mode }) => {
     server: {
       proxy: {
         [env.VITE_APP_BASE_API]: {
-          target: env.VITE_SERVE, // 获取数据的服务器地址
+          target: 'http://sph-api.atguigu.cn', // 获取数据的服务器地址
           changeOrigin: true, // 需要代理跨域
           rewrite: (path) => path.replace(/^\/api/, '') //路径重写
         }
